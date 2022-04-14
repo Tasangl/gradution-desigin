d5df9e9dec18a13bdc5b0bb6b1fe686bcab0c4b5
Tasangl <60890189+Tasangl@users.noreply.github.com>
2022-04-14 21:06:27 +0800

    master

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{info}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data () {
+    return {
+      info: 'Hello'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
