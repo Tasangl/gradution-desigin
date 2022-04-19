f15a618b43ee0608cc6c3b3c0e838c30b8413aef
Tasangl <60890189+Tasangl@users.noreply.github.com>
2022-04-20 01:43:07 +0800

    master

--- api/mock.js
+++ api/mock.js
@@ -1,10 +1,10 @@
 import Mock from 'mockjs'
 import homeApi from './MockServeData/home'
 import userApi from './MockServeData/user'
-import permissionApi from './MockServeData/permission'
+import permissionApi from './MockServeData/permission.js'
 Mock.mock('/home/getData', homeApi.getStatisticalData)
 Mock.mock(/user\/add/, 'post', userApi.createUser)
 Mock.mock(/user\/edit/, 'post', userApi.updateUser)
 Mock.mock(/user\/getUser/, 'get', userApi.getUserList)
 Mock.mock(/user\/del/, 'post', userApi.deleteUser)
-Mock.mock('/permission/getMenu', 'post', permissionApi.getMenu)
+Mock.mock(/permission\/getMenu/, 'post', permissionApi.getMenu)
